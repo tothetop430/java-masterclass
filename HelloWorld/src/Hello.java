1a1ff5707330052064ca399a9c5ea76aea9dd426
tothetop430 <jhuseyrer355545@gmail.com>
2021-12-28 13:31:07 +0530

    update files

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,5 +3,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Siddharth");
+
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
+
+        int expression = (10 + 5) + (2 * 10);
+        System.out.println(expression);
     }
 }
