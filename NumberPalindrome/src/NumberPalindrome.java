4be9ee45ec5f636f50a12301c0cbe13b36172ffa
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-05 18:28:20 +0530

    update files

---
+++ NumberPalindrome/src/NumberPalindrome.java
@@ -0,0 +1,17 @@
+public class NumberPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(0));
+    }
+
+    public static boolean isPalindrome(int number) {
+        int reversed = 0;
+        int tempNumber = Math.abs(number);
+        while (tempNumber>0) {
+            reversed = reversed * 10 + (tempNumber % 10);
+            tempNumber = tempNumber / 10;
+        }
+        return Math.abs(number) == reversed;
+    }
+
+}
