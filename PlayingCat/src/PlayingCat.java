36ee35cab1bfbd50cac20a924d1a7281d2583d8f
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-05 13:28:20 +0530

    update files

--- PlayingCat/src/PlayingCat.java
+++
@@ -1,7 +0,0 @@
-public class PlayingCat {
-
-    public static boolean isCatPlaying(boolean summer, int temperature) {
-        if (summer && temperature>=25 && temperature<=45) return true;
-        else return !summer && temperature >= 25 && temperature <= 35;
-    }
-}
