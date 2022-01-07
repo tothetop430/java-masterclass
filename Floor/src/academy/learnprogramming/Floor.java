0fea7770f1b09a07215b6083825b965fe0df79ad
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-07 15:47:57 +0530

    create Floor

---
+++ Floor/src/academy/learnprogramming/Floor.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+public class Floor {
+
+    private double width, length;
+
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+}
