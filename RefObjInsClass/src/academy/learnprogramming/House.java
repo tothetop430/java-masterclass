88b354a7a9cbdd9cd3493a92c2e31dea6ad227d7
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-08 13:45:00 +0530

    create House

---
+++ RefObjInsClass/src/academy/learnprogramming/House.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming;
+
+public class House {
+
+    private String color;
+
+    public House(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
