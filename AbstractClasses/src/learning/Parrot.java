cd31cc1d4e2f79799ced11fb42b3f7b2ca69bbf5
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-15 12:25:58 +0530

    create bird subclasses

---
+++ AbstractClasses/src/learning/Parrot.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class Parrot extends Bird {
+
+    public Parrot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Parrot can fly");
+    }
+}
---
+++ AbstractClasses/src/learning/Penguin.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class Penguin extends Bird {
+
+    public Penguin(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Penguin cannot fly");
+    }
+}
