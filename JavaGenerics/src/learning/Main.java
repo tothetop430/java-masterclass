33fe25cb94434483249428b0a10721bde0a61b80
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-17 21:54:43 +0530

    update Main

--- JavaGenerics/src/learning/Main.java
+++ JavaGenerics/src/learning/Main.java
@@ -5,16 +5,30 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//
+//        arrayList.add(1);
+//        arrayList.add(2);
+//        arrayList.add(3);
+////        arrayList.add("Sid");
+//        arrayList.add(4);
+//        arrayList.add(5);
+//
+//        printDoubled(arrayList);
 
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-//        arrayList.add("Sid");
-        arrayList.add(4);
-        arrayList.add(5);
 
-        printDoubled(arrayList);
+        FootballPlayer foe = new FootballPlayer("Foe");
+        BaseballPlayer bas = new BaseballPlayer("Bas");
+        SoccerPlayer soc = new SoccerPlayer("Soc");
+
+        Team<FootballPlayer> team = new Team<>("TeaM");
+        team.addPlayer(foe);
+        System.out.println(team.numPlayers());
+
+        Team<BaseballPlayer> team1 = new Team<>("Baseball Team");
+        team1.addPlayer(bas);
+
+
     }
 
     private static void printDoubled(ArrayList<Integer> arrayList) {
