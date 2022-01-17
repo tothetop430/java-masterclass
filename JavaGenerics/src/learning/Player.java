12ba3ead4dcf5ac35a52564855f73e93be041423
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-17 21:54:07 +0530

    create Player

---
+++ JavaGenerics/src/learning/Player.java
@@ -0,0 +1,14 @@
+package learning;
+
+public abstract class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
