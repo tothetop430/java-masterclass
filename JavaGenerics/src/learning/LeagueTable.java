9666ac9316891a6dbe68d3f9dfab07d4e9679092
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-17 22:28:39 +0530

    test LeagueTable

---
+++ JavaGenerics/src/learning/LeagueTable.java
@@ -0,0 +1,28 @@
+package learning;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LeagueTable<T extends Team> {
+    private String name;
+    private ArrayList<T> teamArrayList;
+
+    public LeagueTable(String name) {
+        this.name = name;
+        this.teamArrayList = new ArrayList<>();
+    }
+
+    public void add(T team) {
+        if (teamArrayList.contains(team)) return;
+        teamArrayList.add(team);
+        System.out.println("Added " + team.getName() + " to the league table");
+    }
+
+    public void showLeagueTable() {
+        Collections.sort(teamArrayList);
+        for (T t : teamArrayList) {
+            System.out.println(t.getName() + " -> " + t.ranking());
+        }
+    }
+
+}
--- JavaGenerics/src/learning/Main.java
+++ JavaGenerics/src/learning/Main.java
@@ -16,17 +16,47 @@ public class Main {
 //
 //        printDoubled(arrayList);
 
+//        FootballPlayer foe = new FootballPlayer("Foe");
+//        BaseballPlayer bas = new BaseballPlayer("Bas");
+//        SoccerPlayer soc = new SoccerPlayer("Soc");
+//
+//        Team<FootballPlayer> team = new Team<>("TeaM");
+//        team.addPlayer(foe);
+//        System.out.println(team.numPlayers());
+//
+//        Team<BaseballPlayer> team1 = new Team<>("Baseball Team");
+//        team1.addPlayer(bas);
 
-        FootballPlayer foe = new FootballPlayer("Foe");
-        BaseballPlayer bas = new BaseballPlayer("Bas");
-        SoccerPlayer soc = new SoccerPlayer("Soc");
+        Team<FootballPlayer> team1 = new Team<>("team 1");
+        Team<FootballPlayer> team2 = new Team<>("team 2");
+        Team<FootballPlayer> team3 = new Team<>("team 3");
+        Team<FootballPlayer> team4 = new Team<>("team 4");
 
-        Team<FootballPlayer> team = new Team<>("TeaM");
-        team.addPlayer(foe);
-        System.out.println(team.numPlayers());
+        Team<BaseballPlayer> baseTeam = new Team<>("baseTeam");
 
-        Team<BaseballPlayer> team1 = new Team<>("Baseball Team");
-        team1.addPlayer(bas);
+        LeagueTable<Team<FootballPlayer>> leagueTable = new LeagueTable<>("Football League");
+        leagueTable.add(team1);
+        leagueTable.add(team2);
+        leagueTable.add(team3);
+        leagueTable.add(team4);
+
+        team1.matchResult(team2, 10, 4);
+        team1.matchResult(team3, 0, 4);
+        team1.matchResult(team4, 5, 5);
+
+        team2.matchResult(team1, 1, 1);
+        team2.matchResult(team3, 10, 4);
+        team2.matchResult(team4, 6, 2);
+
+        team3.matchResult(team2, 10, 4);
+        team3.matchResult(team1, 10, 4);
+        team3.matchResult(team4, 5, 5);
+
+        team4.matchResult(team2, 10, 4);
+        team4.matchResult(team3, 10, 4);
+        team4.matchResult(team1, 15, 5);
+
+        leagueTable.showLeagueTable();
 
 
     }
