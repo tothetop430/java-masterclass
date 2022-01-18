cd1426e2f705f5552f1bfe47db73a57edffdfc74
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-18 19:25:40 +0530

    create ScopeChallenge

--- ScopeChallenge/src/learning/Main.java
+++ ScopeChallenge/src/learning/Main.java
@@ -9,12 +9,10 @@ Scope challenge, use 'X'/'x' for all variable/methods/class/instance names (exce
  */
 
 public class Main {
-
     public static void main(String[] args) {
         X x = new X(X());
         x.x();
     }
-
     public static int X() {
         Scanner x = new Scanner(System.in);
         System.out.println("Enter Value of X: ");
