e92b700dcc2155d3075cf02eab686c48f5bd1ada
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-18 12:14:28 +0530

    create Packages.Main

---
+++ Packages/src/learning/Main.java
@@ -0,0 +1,20 @@
+package learning;
+
+// import org.w3c.dom.Node;  // --> needed only if we are not using fully qualified names.
+
+public class Main {
+
+    public static void main(String[] args) {
+        org.w3c.dom.Node node = new org.w3c.dom.Node();     // showing error because Node is an Interface
+
+        /*
+
+        We can use packages using import keyword.
+        Or, we can use fully qualified names.
+        Packages help us to have similar names for classes, interfaces, etc.
+        Only classes/interface specific to a package will be available to that package's scope
+        without needing to actually import those classes.
+
+         */
+    }
+}
