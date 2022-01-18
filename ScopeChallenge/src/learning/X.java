be66f0db76b1f28028b1dec09358dc393a410a4a
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-18 19:24:13 +0530

    create X class

---
+++ ScopeChallenge/src/learning/X.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class X {
+    private final int x;
+    public X(int x) {
+        this.x = x;
+    }
+    public void x() {
+        for (int x=1; x<13; x++) {
+            System.out.println(this.x + " times " + x + " is " + this.x * x);
+        }
+    }
+}
