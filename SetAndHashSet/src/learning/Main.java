1ca5645e42f0fe996031508084fde6e17ed4239f
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-22 19:14:26 +0530

    update Main to deal with HeavenlyBody Key

--- SetAndHashSet/src/learning/Main.java
+++ SetAndHashSet/src/learning/Main.java
@@ -6,48 +6,48 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Map<HeavenlyBody.Key, HeavenlyBody> solarSystem = new HashMap<>();
     private static Set<HeavenlyBody> planets = new HashSet<>();
 
     public static void main(String[] args) {
-        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
-        solarSystem.put(temp.getName(), temp);
+        HeavenlyBody temp = new Planet("Mercury", 88);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
-        HeavenlyBody tempMoon = new HeavenlyBody("Mars Moon", 27);
-        solarSystem.put(tempMoon.getName(), tempMoon);
-        temp.addMoon(tempMoon);
+        HeavenlyBody tempMoon = new Moon("Mars Moon", 27);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
 
-        temp = new HeavenlyBody("Venus", 225);
-        solarSystem.put(temp.getName(), temp);
+        temp = new Planet("Venus", 225);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
-        tempMoon = new HeavenlyBody("Venus Moon", 27);
-        solarSystem.put(tempMoon.getName(), tempMoon);
-        temp.addMoon(tempMoon);
+        tempMoon = new Moon("Venus Moon", 27);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
 
-        temp = new HeavenlyBody("Earth", 365);
-        solarSystem.put(temp.getName(), temp);
+        temp = new Planet("Earth", 365);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
-        tempMoon = new HeavenlyBody("Moon", 27);
-        solarSystem.put(tempMoon.getName(), tempMoon);
-        temp.addMoon(tempMoon);
+        tempMoon = new Moon("Moon", 27);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
 
-        HeavenlyBody earth = new HeavenlyBody("Earth", 365);
+        HeavenlyBody earth = new Planet("Earth", 365);
         planets.add(earth);
 
         Set<HeavenlyBody> moons = new HashSet<>();
 
         System.out.println("Planets");
         for (HeavenlyBody planet: planets) {
-            System.out.println("\t" + planet.getName());
+            System.out.println("\t" + planet.getKey().getName());
             moons.addAll(planet.getSatellites());
         }
 
         System.out.println("Moons");
         for (HeavenlyBody moon: moons) {
-            System.out.println("\t" + moon.getName());
+            System.out.println("\t" + moon.getKey().getName());
         }
 
     }
