9d7fdb529eea2115ab33b2f95d141234fa904827
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-24 18:17:56 +0530

    create Basket

---
+++ SortedCollections/src/learning/Basket.java
@@ -0,0 +1,39 @@
+package learning;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Basket {
+
+    private final String name;
+    private final Map<StockItem, Integer> list;
+
+    public Basket(String name) {
+        this.name = name;
+        this.list = new HashMap<>();
+    }
+
+    public int addToBasket(StockItem item, int qty) {
+        if (item != null && qty > 0) {
+            int inBasket = this.list.getOrDefault(item, 0);
+            this.list.put(item, inBasket + qty);
+            return inBasket;
+        }
+        return 0;
+    }
+
+    public Map<StockItem, Integer> getItems() {
+        return Collections.unmodifiableMap(this.list);
+    }
+
+    @Override
+    public String toString() {
+        String basketList = "";
+        for (Map.Entry<StockItem, Integer> item: list.entrySet()) {
+            basketList += item.getKey() + " purchasing -> " + item.getValue() + "\n";
+            basketList += "Total Price: " + item.getKey().getPrice() * item.getValue();
+        }
+        return basketList;
+    }
+}
