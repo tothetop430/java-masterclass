21d81937c3b98369db1ce1fd90de9bcee9c21568
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-24 21:07:22 +0530

    update files

--- SortedCollections/src/learning/StockList.java
+++ SortedCollections/src/learning/StockList.java
@@ -15,16 +15,16 @@ public class StockList {
     public int addStock(StockItem item) {
         if (item != null) {
             StockItem inStock = this.list.getOrDefault(item.getName(), item);
-            if (inStock != item) inStock.adjustQuantity(item.getQuantity());
+            if (inStock != item) inStock.adjustQuantity(item.availableQty());
             this.list.put(inStock.getName(), inStock);
-            return inStock.getQuantity();
+            return inStock.availableQty();
         }
         return 0;
     }
 
     public int sellStock(String itemName, int qty) {
         StockItem inStock = this.list.getOrDefault(itemName, null);
-        if (inStock != null && qty > 0 && inStock.getQuantity() >= qty) {
+        if (inStock != null && qty > 0 && inStock.availableQty() >= qty) {
             inStock.adjustQuantity(-1 * qty);
             return qty;
         }
@@ -44,8 +44,8 @@ public class StockList {
         String itemList = "";
         for (String itemName : this.list.keySet()) {
             itemList += this.list.get(itemName);
-            itemList += "Total Price for " + itemName + " with " + this.list.get(itemName).getQuantity() +
-                    " qty. is " + (this.list.get(itemName).getQuantity() *
+            itemList += "Total Price for " + itemName + " with " + this.list.get(itemName).availableQty() +
+                    " qty. is " + (this.list.get(itemName).availableQty() *
                     this.list.get(itemName).getPrice()) + "\n";
         }
         return itemList;
@@ -53,7 +53,7 @@ public class StockList {
 
     public int removeStock(StockItem item) {
         if (item != null) {
-            int qty = item.getQuantity();
+            int qty = item.availableQty();
             this.list.remove(item.getName());
             return qty;
         }
