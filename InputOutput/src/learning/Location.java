0e6ce1c568da2295405795e8c91d741782b9820c
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-10 14:35:35 +0530

    using Serializable interface

--- InputOutput/src/learning/Location.java
+++ InputOutput/src/learning/Location.java
@@ -1,9 +1,13 @@
 package learning;
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Location {
+public class Location implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
