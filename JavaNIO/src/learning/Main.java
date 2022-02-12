56c13ad35cf863870d33274245fa995170539114
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-12 12:38:25 +0530

    update JavaNIO.Main

--- JavaNIO/src/learning/Main.java
+++ JavaNIO/src/learning/Main.java
@@ -1,7 +1,9 @@
 package learning;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
@@ -13,9 +15,26 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()) {
+            byte[] bytes = "Hello World!".getBytes(StandardCharsets.UTF_8);
+            ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+            int writtenBytes = binChannel.write(byteBuffer);
+            System.out.println("Bytes Written " + writtenBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(42);
+            intBuffer.flip();
+            writtenBytes = binChannel.write(intBuffer);
+            System.out.println("Integer Bytes Written " + writtenBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-9823); intBuffer.flip();
+            writtenBytes = binChannel.write(intBuffer);
+            System.out.println("Negative Integer Buffer Written " + writtenBytes);
+        }
 //        FileInputStream fileInputStream = new FileInputStream("data.txt");
 //        FileChannel fileChannel = fileInputStream.getChannel();
-
         Path filePath = FileSystems.getDefault().getPath("data.txt");
         Files.write(filePath, "\nFile Line 5".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
         List<String> lines = Files.readAllLines(filePath);
