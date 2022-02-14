0cde7b5334ca45e6a0189397c8653a38c8deeb28
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-14 21:06:00 +0530

    use Thread sleep

--- Threads/src/learning/AnotherThread.java
+++ Threads/src/learning/AnotherThread.java
@@ -4,6 +4,15 @@ public class AnotherThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Hello from the AnotherThread.");
+        System.out.println("Hello from " + currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("Another thread woke me up.");
+            return;
+        }
+
+        System.out.println("5 Seconds have passed.");
+
     }
 }
