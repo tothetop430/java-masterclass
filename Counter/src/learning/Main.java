35d8a997e19be946f7c35fd84439fc147de0f73c
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-14 22:07:55 +0530

    create Countdown and CountdownThread

---
+++ Counter/src/learning/Main.java
@@ -0,0 +1,52 @@
+package learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Countdown countdown = new Countdown();
+
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread 1");
+
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Countdown {
+    private int i;  // async variable
+//    private String color;
+    public void doCountdown() {
+        String color;
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            default:
+                color = ThreadColor.ANSI_GREEN;
+                break;
+        }
+        for (i=10; i>0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + " : i = " + i);
+        }
+    }
+}
+
+class CountdownThread extends Thread {
+    private final Countdown countdown;
+
+    public CountdownThread(Countdown countdown) {
+        this.countdown = countdown;
+    }
+
+    @Override
+    public void run() {
+        this.countdown.doCountdown();
+    }
+}
