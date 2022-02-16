be9596fddcef9d63e14d27dbe73c3a94c7a137ec
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-16 16:34:38 +0530

    using executor service thread pool

--- ProducerConsumer/src/learning/Main.java
+++ ProducerConsumer/src/learning/Main.java
@@ -3,6 +3,7 @@ package learning;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -10,13 +11,33 @@ public class Main {
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<>();
         ReentrantLock bufferLock = new ReentrantLock();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+//        new Thread(producer).start();
+//        new Thread(consumer1).start();
+//        new Thread(consumer2).start();
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_PURPLE + "Inside Callable Future");
+                return "This is returned from Callable";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException | InterruptedException ignored) {}
+
+        executorService.shutdown();
     }
 }
 
