c5462d6f70ef19a4a3f743af8971077432e119c4
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-17 15:53:34 +0530

    create BankAccountChallenge.Main

---
+++ BankAccountChallenge/src/learning/Main.java
@@ -0,0 +1,42 @@
+package learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("1234-50", 1000);
+        new Thread(new Person1(bankAccount)).start();
+        new Thread(new Person2(bankAccount)).start();
+    }
+
+    private static class Person1 implements Runnable {
+        private final BankAccount bankAccount;
+        public Person1(BankAccount bankAccount) {
+            this.bankAccount = bankAccount;
+        }
+        @Override
+        public void run() {
+            System.out.println("Person1: Depositing 300");
+            bankAccount.deposit(300);
+            System.out.println("Available Balance: " + bankAccount.getBalance());
+            System.out.println("Person1: Withdrawing 50");
+            bankAccount.withdraw(50);
+            System.out.println("Available Balance: " + bankAccount.getBalance());
+        }
+    }
+
+    private static class Person2 implements Runnable {
+        private final BankAccount bankAccount;
+        public Person2(BankAccount bankAccount) {
+            this.bankAccount = bankAccount;
+        }
+        @Override
+        public void run() {
+            System.out.println("Person2: Depositing 203.75");
+            bankAccount.deposit(203.75);
+            System.out.println("Available Balance: " + bankAccount.getBalance());
+            System.out.println("Person2: Withdrawing 100");
+            bankAccount.withdraw(100);
+            System.out.println("Available Balance: " + bankAccount.getBalance());
+        }
+    }
+}
