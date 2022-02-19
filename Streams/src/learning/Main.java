de84a188c2675da57157709760361501c921b0c0
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-19 21:04:25 +0530

    using streams

---
+++ Streams/src/learning/Main.java
@@ -0,0 +1,18 @@
+package learning;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> aList = Arrays.asList(
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
+        );
+
+        aList.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.compareTo("E") > 0)
+                .forEach(System.out::println);
+    }
+}
