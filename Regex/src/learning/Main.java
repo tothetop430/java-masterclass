88576048b32c806f0d58583d4cc6292896b7e3c6
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-21 15:35:01 +0530

    using matches

--- Regex/src/learning/Main.java
+++ Regex/src/learning/Main.java
@@ -12,9 +12,9 @@ public class Main {
         System.out.println(anotherGibberish.replaceAll(".", "Y"));
         System.out.println(anotherGibberish.replaceAll("^abcDee", "YYY"));
 
-//        System.out.println("****************************************");
-//        System.out.println(anotherGibberish.matches("^abcDeee"));
-//        System.out.println(anotherGibberish.matches("^abcDeeeF123ghIabcDeee"));
+        System.out.println("****************************************");
+        System.out.println(anotherGibberish.matches("^abcDeee"));
+        System.out.println(anotherGibberish.matches("^abcDeeeF123ghIabcDeee"));
 //        System.out.println(anotherGibberish.replaceAll("abcDeee$", "0END"));
 //        System.out.println(anotherGibberish.replaceAll("[abc]", "Z"));
 //        System.out.println(anotherGibberish.replaceAll("[abc][D]", "Z"));
