cf158be46e61c432a0be3503f8219ee1b33c1085
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-26 16:54:37 +0530

    using beforeeach and afterall

--- Banking/src/learning/BankAccountTest.java
+++ Banking/src/learning/BankAccountTest.java
@@ -1,53 +1,60 @@
 package learning;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BankAccountTest {
 
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    public void setup() {
+        bankAccount = new BankAccount("ABC", "DEF",
+                1000.00, BankAccount.CHECKING);
+        System.out.println("Tests Running...");
+    }
+
     @Test
     void deposit() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF",
-                1000.00, BankAccount.CHECKING);
         double balance = bankAccount.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
     }
 
     @Test
     void withdraw() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF",
-                1000.00, BankAccount.CHECKING);
         double balance = bankAccount.withdraw(200.00, true);
         assertEquals(800.00, balance, 0);
     }
 
     @Test
     void getBalance_deposit() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF",
-                1000.00, BankAccount.CHECKING);
         bankAccount.deposit(200.00, true);
         assertEquals(1200.00, bankAccount.getBalance(), 0);
     }
 
     @Test
     void getBalance_withdraw() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF",
-                1000.00, BankAccount.CHECKING);
         bankAccount.withdraw(200.00, true);
         assertEquals(800.00, bankAccount.getBalance(), 0);
     }
 
     @Test
     public void isChecking_true() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF",
-                1000.00, BankAccount.CHECKING);
         assertTrue(bankAccount.isChecking());
     }
 
     @Test
     public void isChecking_false() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF",
+        bankAccount = new BankAccount("ABC", "DEF",
                 1000.00, BankAccount.SAVINGS);
         assertFalse(bankAccount.isChecking());
+    }
+
+    @AfterAll
+    static void shutDown() {
+        System.out.println("Shutting down unit test cases.");
     }
 }
