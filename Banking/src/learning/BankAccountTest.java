cab0f7485b1645ae71f7b4c08755c2c992134d89
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-26 15:26:47 +0530

    add CHECKING/SAVINGS account type

--- Banking/src/learning/BankAccountTest.java
+++ Banking/src/learning/BankAccountTest.java
@@ -7,29 +7,43 @@ class BankAccountTest {
 
     @Test
     void deposit() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF", 1000.00);
+        BankAccount bankAccount = new BankAccount("ABC", "DEF",
+                1000.00, BankAccount.CHECKING);
         double balance = bankAccount.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
     }
 
     @Test
     void withdraw() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF", 1000.00);
+        BankAccount bankAccount = new BankAccount("ABC", "DEF",
+                1000.00, BankAccount.CHECKING);
         double balance = bankAccount.withdraw(200.00, true);
         assertEquals(800.00, balance, 0);
     }
 
     @Test
     void getBalance_deposit() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF", 1000.00);
+        BankAccount bankAccount = new BankAccount("ABC", "DEF",
+                1000.00, BankAccount.CHECKING);
         bankAccount.deposit(200.00, true);
         assertEquals(1200.00, bankAccount.getBalance(), 0);
     }
 
     @Test
     void getBalance_withdraw() {
-        BankAccount bankAccount = new BankAccount("ABC", "DEF", 1000.00);
+        BankAccount bankAccount = new BankAccount("ABC", "DEF",
+                1000.00, BankAccount.CHECKING);
         bankAccount.withdraw(200.00, true);
         assertEquals(800.00, bankAccount.getBalance(), 0);
+    }
+
+    @Test
+    public void isChecking_true() {
+
+    }
+
+    @Test
+    public void isChecking_false() {
+
     }
 }
