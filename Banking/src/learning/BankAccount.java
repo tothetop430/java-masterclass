2eddb1066395ff14e273ec78261b330bd6d18e04
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-26 15:25:54 +0530

    add CHECKING/SAVINGS account type

--- Banking/src/learning/BankAccount.java
+++ Banking/src/learning/BankAccount.java
@@ -2,14 +2,20 @@ package learning;
 
 public class BankAccount {
 
-    private String firstName;
-    private String lastName;
+    private final String firstName;
+    private final String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private final int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int accountType) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = accountType;
     }
 
     public double deposit(double amount, boolean branch) {
@@ -25,6 +31,14 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public int getAccountType() {
+        return accountType;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
     }
 
     //More methods as required below
