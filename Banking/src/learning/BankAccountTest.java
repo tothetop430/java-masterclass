657a97820bfc48f70882480c188b8e957eb03189
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-26 14:36:38 +0530

    create BankAccountTest to fail

---
+++ Banking/src/learning/BankAccountTest.java
@@ -0,0 +1,22 @@
+package learning;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+
+    @Test
+    void deposit() {
+        fail("Failing deposit method");
+    }
+
+    @Test
+    void withdraw() {
+        fail("Failing withdraw method");
+    }
+
+    @Test
+    void getBalance() {
+        fail("Failing getBalance method");
+    }
+}
