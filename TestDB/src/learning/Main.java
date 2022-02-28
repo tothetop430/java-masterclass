4c672e33cb8710c53aa07466e555ab7670feeb39
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-28 22:25:30 +0530

    using Connection and Statement objects

--- TestDB/src/learning/Main.java
+++ TestDB/src/learning/Main.java
@@ -10,7 +10,9 @@ public class Main {
     public static void main(String[] args) {
 	    try(Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/siddharth/Documents/Projects/java-masterclass/TestDB/testjava.db");
             Statement statement = connection.createStatement()) {
+
             statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
         } catch (SQLException e) {
             System.out.println("Database Exception caught: " + e.getMessage());
         }
