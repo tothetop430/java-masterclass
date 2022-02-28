4393acbf99c768fcb5e9bd2ee22e10d9d8d6bcb8
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-28 22:41:35 +0530

    inserting data and setAutoCommit

--- TestDB/src/learning/Main.java
+++ TestDB/src/learning/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
 	    try(Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/siddharth/Documents/Projects/java-masterclass/TestDB/testjava.db");
             Statement statement = connection.createStatement()) {
-            connection.setAutoCommit(false);
+//            connection.setAutoCommit(false);
             statement.execute("CREATE TABLE IF NOT EXISTS" +
                     " contacts (name TEXT, phone INTEGER, email TEXT)");
-            statement.execute("INSERT INTO contacts values('Siddharth', 123456, 'sid@email.com')");
+            statement.execute("INSERT INTO contacts values('Abhishek', 123457, 'abhi@email.com')");
         } catch (SQLException e) {
             System.out.println("Database Exception caught: " + e.getMessage());
         }
