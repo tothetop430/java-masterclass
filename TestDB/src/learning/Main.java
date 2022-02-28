765bcc79b4bd5c0f499ecc773283488ae00917ba
tothetop430 <jhuseyrer355545@gmail.com>
2022-02-28 22:59:25 +0530

    updating and deleting from DB

--- TestDB/src/learning/Main.java
+++ TestDB/src/learning/Main.java
@@ -13,7 +13,13 @@ public class Main {
 //            connection.setAutoCommit(false);
             statement.execute("CREATE TABLE IF NOT EXISTS" +
                     " contacts (name TEXT, phone INTEGER, email TEXT)");
-            statement.execute("INSERT INTO contacts values('Abhishek', 123457, 'abhi@email.com')");
+//            statement.execute("INSERT INTO contacts values('Tanu', 123458, 'tanu@email.com')");
+//            statement.execute("INSERT INTO contacts values('Mem1', 123459, 'mem1@email.com')");
+//            statement.execute("INSERT INTO contacts values('Mem2', 123450, 'mem2@email.com')");
+//            statement.execute("UPDATE contacts SET phone=123454 WHERE name='Siddharth'");
+//            statement.execute("DELETE FROM contacts WHERE name='Mem2'");
+
+
         } catch (SQLException e) {
             System.out.println("Database Exception caught: " + e.getMessage());
         }
