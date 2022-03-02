8a8967935ae3fd79d81889e28228fe499acdc22e
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-02 23:51:22 +0530

    testing openConnection and closeConnection

--- Music/src/learning/Main.java
+++ Music/src/learning/Main.java
@@ -3,6 +3,14 @@ package learning;
 public class Main {
 
     public static void main(String[] args) {
-
+        DataSource dataSource = new DataSource();
+        if (!dataSource.openConnection()) {
+            System.out.println("Failed to Open DataSource Connection!");
+        }
+        else {
+            System.out.println("Establised DataSource Connection");
+            dataSource.closeConnection();
+            System.out.println("Finally Closed DataSource Connection");
+        }
     }
 }
