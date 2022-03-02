29a1af4132a8ac18f905023ce5c9538aadc8b6e1
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-02 23:09:02 +0530

    using constants for table/column names

--- TestDB/src/learning/Main.java
+++ TestDB/src/learning/Main.java
@@ -4,27 +4,31 @@ import java.sql.*;
 
 public class Main {
 
+    public static final String connectionString = "jdbc:sqlite:/Users/siddharth/Documents/Projects/java-masterclass/TestDB/testjava.db";
+
+    public static final String TABLE_NAME = "contacts";
+    public static final String COL_NAME = "name";
+    public static final String COL_PHONE = "phone";
+    public static final String COL_EMAIL = "email";
+
     public static void main(String[] args) {
-	    try(Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/siddharth/Documents/Projects/java-masterclass/TestDB/testjava.db");
+	    try(Connection connection = DriverManager.getConnection(connectionString);
             Statement statement = connection.createStatement()) {
-//            connection.setAutoCommit(false);
-            statement.execute("CREATE TABLE IF NOT EXISTS" +
-                    " contacts (name TEXT, phone INTEGER, email TEXT)");
-//            statement.execute("INSERT INTO contacts values('Tanu', 123458, 'tanu@email.com')");
-//            statement.execute("INSERT INTO contacts values('Mem1', 123459, 'mem1@email.com')");
-//            statement.execute("INSERT INTO contacts values('Mem2', 123450, 'mem2@email.com')");
-//            statement.execute("UPDATE contacts SET phone=123454 WHERE name='Siddharth'");
-//            statement.execute("DELETE FROM contacts WHERE name='Mem2'");
+            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                    " (" + COL_NAME + " TEXT, " + COL_PHONE + " INTEGER, " + COL_EMAIL + " TEXT)");
+
+            statement.execute("INSERT INTO " + TABLE_NAME + " values(" +
+                    "'mem1'" + ", " + "123" + ", " + "'mem1@email.com'" +
+                    ")");
 
 //            statement.execute("SELECT * FROM contacts");
 //            ResultSet results = statement.getResultSet();
-            ResultSet results = statement.executeQuery("SELECT * FROM contacts");
+            ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_NAME);
             while (results.next()) {
-                System.out.println(results.getString("name") + " "
-                        + results.getInt("phone") + " "
-                        + results.getString("email"));
+                System.out.println(results.getString(COL_NAME) + " "
+                        + results.getInt(COL_PHONE) + " "
+                        + results.getString(COL_EMAIL));
             }
-
             results.close();
         } catch (SQLException e) {
             System.out.println("Database Exception caught: " + e.getMessage());
