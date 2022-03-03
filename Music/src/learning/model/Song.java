5855f6d98df5e186dc467d01e22bb66dfb20ac32
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-03 15:05:16 +0530

    create Song

---
+++ Music/src/learning/model/Song.java
@@ -0,0 +1,41 @@
+package learning.model;
+
+public class Song {
+
+    private int id;
+    private int track;
+    private String title;
+    private int albumid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAlbumid() {
+        return albumid;
+    }
+
+    public void setAlbumid(int albumid) {
+        this.albumid = albumid;
+    }
+}
