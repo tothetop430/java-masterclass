7c5fa0287e38a3af1a6b125dda0d264a347c9c08
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-03 15:05:04 +0530

    create Album

---
+++ Music/src/learning/model/Album.java
@@ -0,0 +1,32 @@
+package learning.model;
+
+public class Album {
+
+    private int id;
+    private String name;
+    private int artistid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArtistid() {
+        return artistid;
+    }
+
+    public void setArtistid(int artistid) {
+        this.artistid = artistid;
+    }
+}
