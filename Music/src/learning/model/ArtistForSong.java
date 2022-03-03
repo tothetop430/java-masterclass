201dca036220e3b71ceec7dab9fe4092e8e8721a
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-03 18:23:30 +0530

    create ArtistForSong

---
+++ Music/src/learning/model/ArtistForSong.java
@@ -0,0 +1,32 @@
+package learning.model;
+
+public class ArtistForSong {
+
+    private String albumName;
+    private String artistName;
+    private String titleTrack;
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getTitleTrack() {
+        return titleTrack;
+    }
+
+    public void setTitleTrack(String titleTrack) {
+        this.titleTrack = titleTrack;
+    }
+}
